a181ad7be47e5a68b2f2bd33d1de8abed6ba9b56
benson09121 <benson09.javier@gmail.com>
2025-02-11 20:32:32 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+require('dotenv').config();
+// load environment variables
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
+})
+
+
+
+app.get('/', (req, res) => {
+    con.query('SELECT * FROM tbl_user', (err, rows) => {
+        if (err) throw err;
+        res.json(rows);
+    })
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+})
